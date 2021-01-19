f7ba72b392e0f6426c3df6294c4c94adeb2d7167
dangtdn <nguyendanglop93@gmail.com>
2021-01-19 08:21:54 +0700

    myFrontEnd_CoBan

---
+++ BT_Bootstrap/Nguyen_Nhat_Dang/Camping/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.content-item h2').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
---
+++ BT_Bootstrap/Nguyen_Nhat_Dang/HomeStead/js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  $(".owl-carousel").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
